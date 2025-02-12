772c4abea9b1e5d0a2f6a53c2c62a150f977c47e
Armstrong Omoregie <ammycan6@gmail.com>
2025-02-12 16:09:06 +0100

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  console.log(process.env.REACT_APP_WEATHER_API);
+  return (
+    <div className="App">
+      <h1>Test</h1>
+    </div>
+  );
+}
+
+export default App;
